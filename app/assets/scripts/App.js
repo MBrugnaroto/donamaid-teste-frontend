624a44a3de8eee4838f677cf9fd78129e0547e1a
MBrugnaroto <brugnaroto@gmail.com>
2020-07-20 15:52:52 -0300

    FEAT. Extração, carga e transformação dos dados concluida.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,13 @@
 import '../styles/styles.css'
 
 import modal from './modules/modal';
+import insertData from './modules/insertData';
+import consumeSWAPI from './modules/consumeSWAPI';
 
 new modal();
+let data = new consumeSWAPI();
+new insertData(data);
+
+
+
+
